2e36a44518e3e186b7384cdb28b2d925181577f8
Joao Pedro PC-Vicky <joaocedan2002@gmail.com>
2022-12-26 19:43:20 -0300

    [STYLE] Correção de importações

--- src/screens/Dashboard/CashInList/CashOutList.tsx
+++ src/screens/Dashboard/CashInList/CashOutList.tsx
@@ -1,6 +1,4 @@
-
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View, Text, ScrollView } from "react-native";
 import Layout from "../../../components/Layout";
 import { styles } from "./styles";
